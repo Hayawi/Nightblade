b1e0a6a3febe5b642a79e0dc252fc871c586e236
Hayawi <yahya.ismail@hotmail.ca>
2015-06-25 20:29:04 -0700

    Added a player class and an entities package to store entities

---
+++ core/src/com/nightblade/entities/Player.java
@@ -0,0 +1,59 @@
+package com.nightblade.entities;
+
+public class Player {
+
+	private int x;
+	private int y;
+	private int stamina;
+	private int health;
+	
+	/**
+	 * @return the x
+	 */
+	public int getX() {
+		return x;
+	}
+	/**
+	 * @param x the x to set
+	 */
+	public void setX(int x) {
+		this.x = x;
+	}
+	/**
+	 * @return the y
+	 */
+	public int getY() {
+		return y;
+	}
+	/**
+	 * @param y the y to set
+	 */
+	public void setY(int y) {
+		this.y = y;
+	}
+	/**
+	 * @return the stamina
+	 */
+	public int getStamina() {
+		return stamina;
+	}
+	/**
+	 * @param stamina the stamina to set
+	 */
+	public void setStamina(int stamina) {
+		this.stamina = stamina;
+	}
+	/**
+	 * @return the health
+	 */
+	public int getHealth() {
+		return health;
+	}
+	/**
+	 * @param health the health to set
+	 */
+	public void setHealth(int health) {
+		this.health = health;
+	}
+	
+}
